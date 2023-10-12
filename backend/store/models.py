99409f5d70b742b2bee38e4078d0a9017bdaee92
SandaruwanWGMA <molinduachintha24@gmail.com>
2023-10-12 14:05:19 +0530

    created migrations

--- backend/store/models.py
+++ backend/store/models.py
@@ -11,4 +11,8 @@ class Bill(models.Model):
     # bill_id = models.CharField(primary_key=True, max_length=7)
     product_id = models.ForeignKey("Product", on_delete=models.RESTRICT)
     total_list_price = models.DecimalField(max_digits=8, decimal_places=2)
-    discount_percent = models.DecimalField(max_digits=2)
+    discount_percent = models.IntegerField(max_length=2)
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=255)
