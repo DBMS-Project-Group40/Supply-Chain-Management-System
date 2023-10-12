6f404cc761c8fb35ba3cd621f194bfe244694140
SandaruwanWGMA <molinduachintha24@gmail.com>
2023-10-12 15:02:01 +0530

    commit

--- backend/store/models.py
+++ backend/store/models.py
@@ -2,17 +2,18 @@ from django.db import models
 
 
 # Create your models here.
-class Order(models.Model):
-    # order_id = models.CharField(primary_key=True, max_length=7)
-    bill_id = models.ForeignKey("Bill", on_delete=models.CASCADE)
+# class Order(models.Model):
+#     # order_id = models.CharField(primary_key=True, max_length=7)
+#     bill_id = models.ForeignKey("Bill", on_delete=models.CASCADE)
 
 
-class Bill(models.Model):
-    # bill_id = models.CharField(primary_key=True, max_length=7)
-    product_id = models.ForeignKey("Product", on_delete=models.RESTRICT)
-    total_list_price = models.DecimalField(max_digits=8, decimal_places=2)
-    discount_percent = models.IntegerField(max_length=2)
+# class Bill(models.Model):
+#     # bill_id = models.CharField(primary_key=True, max_length=7)
+#     # id = models.ForeignKey("Product", on_delete=models.RESTRICT)
+#     total_list_price = models.DecimalField(max_digits=8, decimal_places=2)
+#     discount_percent = models.xIntegerField()
 
 
 class Product(models.Model):
+    # id = models.IntegerField(primary_key=True)
     product_name = models.CharField(max_length=255)
--- backend/store/views.py
+++ backend/store/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.mixins import ListModelMixin
+from .models import Product
+from .serializations import ProductSerializer
+
 
 # Create your views here.
+@api_view(["GET", "POST"])
+def product_list(request):
+    if request.method == "GET":
+        queryset = Product.objects.all()
+        serializer = ProductSerializer(
+            queryset, many=True, context={"request": request}
+        )
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = ProductSerializer(data=request.data)
+        # serializer.is_valid(raise_exception=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(request.data, status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
