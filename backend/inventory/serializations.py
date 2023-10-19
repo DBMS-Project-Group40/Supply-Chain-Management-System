133f909f1b22847f37e98a1f36c262897a980f81
SandaruwanWGMA <molinduachintha24@gmail.com>
2023-10-19 14:45:40 +0530

    commit

--- backend/inventory/serializations.py
+++ backend/inventory/serializations.py
@@ -1,14 +1,26 @@
 from rest_framework import serializers
-from .models import Product, Bill
+from .models import Product, TrainSchedule, Order, Bill
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ["ProductID", "product_name"]
+        fields = "__all__"
+
+
+class TrainScheduleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TrainSchedule
+        fields = "__all__"
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = "__all__"
 
 
 class BillSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bill
-        fields = ["BillID", "total_list_price"]
+        fields = "__all__"
