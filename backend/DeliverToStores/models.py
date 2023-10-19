1008e3a36b8d20860d61957422d7ecbc8872e63c
SandaruwanWGMA <molinduachintha24@gmail.com>
2023-10-19 14:48:21 +0530

    commit

--- backend/DeliverToStores/models.py
+++ backend/DeliverToStores/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Store(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+
+
+class Truck(models.Model):
+    name = models.CharField(max_length=255)
+    capacity = models.PositiveIntegerField()
+
+
+class TruckSchedule(models.Model):
+    truck = models.ForeignKey(Truck, on_delete=models.CASCADE)
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+
+
+class TruckRoute(models.Model):
+    schedule = models.ForeignKey(TruckSchedule, on_delete=models.CASCADE)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+
+
+class TruckGoods(models.Model):
+    route = models.ForeignKey(TruckRoute, on_delete=models.CASCADE)
+    product = models.CharField(max_length=255)
+    quantity = models.PositiveIntegerField()
