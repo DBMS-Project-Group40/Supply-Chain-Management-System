a693580a4b6f6a2e34fb0551ea9d003679e0976f
SandaruwanWGMA <molinduachintha24@gmail.com>
2023-10-19 14:58:49 +0530

    commit

---
+++ backend/DeliveryToShops/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("shop-orders/", views.shop_order_list),
+    path("shop-orders/<slug:slug>/", views.shop_order_detail),
+    path("shop-deliveries/", views.shop_delivery_list),
+    path("shop-deliveries/<slug:slug>/", views.shop_delivery_detail),
+]
