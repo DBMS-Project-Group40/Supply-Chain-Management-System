da11f8febc7a129f9dca1f4f9f20a7b05dc33996
SandaruwanWGMA <molinduachintha24@gmail.com>
2023-10-19 14:52:37 +0530

    commit

--- backend/DeliveryToShops/models.py
+++ backend/DeliveryToShops/models.py
@@ -5,8 +5,10 @@ class ShopOrder(models.Model):
     store = models.ForeignKey("delivery_to_stores.Store", on_delete=models.CASCADE)
     product = models.CharField(max_length=255)
     quantity = models.PositiveIntegerField()
+    slug = models.SlugField()
 
 
 class ShopDelivery(models.Model):
     order = models.ForeignKey(ShopOrder, on_delete=models.CASCADE)
     delivery_time = models.DateTimeField()
+    slug = models.SlugField()
