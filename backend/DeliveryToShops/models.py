4381b6648e38a7c3f68f0d7ab4043197c9e086f2
SandaruwanWGMA <molinduachintha24@gmail.com>
2023-10-19 14:48:46 +0530

    commit

--- backend/DeliveryToShops/models.py
+++ backend/DeliveryToShops/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class ShopOrder(models.Model):
+    store = models.ForeignKey("delivery_to_stores.Store", on_delete=models.CASCADE)
+    product = models.CharField(max_length=255)
+    quantity = models.PositiveIntegerField()
+
+
+class ShopDelivery(models.Model):
+    order = models.ForeignKey(ShopOrder, on_delete=models.CASCADE)
+    delivery_time = models.DateTimeField()
