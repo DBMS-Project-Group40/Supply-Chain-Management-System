3978736d9b00da170f52bd2334ebf24e8b320b3f
SandaruwanWGMA <molinduachintha24@gmail.com>
2023-10-20 08:34:42 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,27 @@
-import React from "react";
+import { useState } from "react";
+import "./App.css";
+import Header from "./Header";
+import Sidebar from "./SideBar";
+import Home from "./Home";
+import "./index.css";
 
-function App(props) {
-  return <div>App</div>;
+function App() {
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false);
+
+  const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle);
+  };
+
+  return (
+    <div className="grid-container">
+      <Header OpenSidebar={OpenSidebar} />
+      <Sidebar
+        openSidebarToggle={openSidebarToggle}
+        OpenSidebar={OpenSidebar}
+      />
+      <Home />
+    </div>
+  );
 }
 
 export default App;
