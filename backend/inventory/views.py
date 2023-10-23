6665c807ebd4b62844c7f421b16b37a1666122ec
SandaruwanWGMA <molinduachintha24@gmail.com>
2023-10-23 20:04:54 +0530

    commit

--- backend/inventory/views.py
+++ backend/inventory/views.py
@@ -1,153 +1,196 @@
-from django.shortcuts import render, get_object_or_404
-from rest_framework import status
-from rest_framework.response import Response
+from django.http import JsonResponse, HttpResponse
+from django.db import connection
 from rest_framework.decorators import api_view
-from .models import Product, TrainSchedule, Order, Bill
-from .serializations import (
-    ProductSerializer,
-    TrainScheduleSerializer,
-    OrderSerializer,
-    BillSerializer,
-)
+from rest_framework import status
+
+
+def dictfetchall(cursor):
+    """Return all rows from a cursor as a dict"""
+    columns = [col[0] for col in cursor.description]
+    return [dict(zip(columns, row)) for row in cursor.fetchall()]
 
 
 @api_view(["GET", "POST"])
 def product_list(request):
     if request.method == "GET":
-        queryset = Product.objects.all()
-        serializer = ProductSerializer(
-            queryset, many=True, context={"request": request}
-        )
-        return Response(serializer.data)
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM product")
+            products = dictfetchall(cursor)
+        return JsonResponse(products, safe=False)
     elif request.method == "POST":
-        serializer = ProductSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(request.data, status=status.HTTP_200_OK)
-        else:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+        with connection.cursor() as cursor:
+            cursor.execute(
+                "INSERT INTO product (name, description) VALUES (%s, %s)",
+                [request.data.get("name"), request.data.get("description")],
+            )
+        return HttpResponse(status=status.HTTP_200_OK)
 
 
 @api_view(["GET", "PUT", "DELETE"])
 def product_detail(request, id):
-    product = get_object_or_404(Product, pk=id)
-    if request.method == "GET":
-        serializer = ProductSerializer(product, context={"request": request})
-        return Response(serializer.data)
-    elif request.method == "PUT":
-        serializer = ProductSerializer(product, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-    elif request.method == "DELETE":
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    with connection.cursor() as cursor:
+        if request.method == "GET":
+            cursor.execute("SELECT * FROM product WHERE id = %s", [id])
+            product = cursor.fetchone()
+            if product:
+                return JsonResponse(
+                    dict(zip([col[0] for col in cursor.description], product))
+                )
+            else:
+                return HttpResponse(status=404)
+        elif request.method == "PUT":
+            cursor.execute(
+                "UPDATE product SET name = %s, description = %s WHERE id = %s",
+                [request.data.get("name"), request.data.get("description"), id],
+            )
+            return HttpResponse(status=status.HTTP_200_OK)
+        elif request.method == "DELETE":
+            cursor.execute("DELETE FROM product WHERE id = %s", [id])
+            return HttpResponse(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view(["GET"])
 def product_count(request):
     if request.method == "GET":
-        count = Product.objects.all().count()
-        return Response({"count": count}, status=status.HTTP_200_OK)
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT COUNT(*) FROM product")
+            count = cursor.fetchone()[0]
+        return JsonResponse({"count": count})
 
 
 @api_view(["GET"])
 def num_of_product_categories(request):
     if request.method == "GET":
-        distinct_categories = (
-            Product.objects.values_list("product_name", flat=True).distinct().count()
-        )
-        return Response({"categories": distinct_categories}, status=status.HTTP_200_OK)
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT COUNT(DISTINCT product_name) FROM product")
+            distinct_categories = cursor.fetchone()[0]
+        return JsonResponse({"categories": distinct_categories})
 
 
 @api_view(["GET", "POST"])
 def train_schedule_list(request):
     if request.method == "GET":
-        queryset = TrainSchedule.objects.all()
-        serializer = TrainScheduleSerializer(queryset, many=True)
-        return Response(serializer.data)
-    if request.method == "POST":
-        serializer = TrainScheduleSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(request.data, status=status.HTTP_200_OK)
-        else:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM yourapp_trainschedule")
+            schedules = dictfetchall(cursor)
+        return JsonResponse(schedules, safe=False)
+    elif request.method == "POST":
+        with connection.cursor() as cursor:
+            # Assuming TrainSchedule has fields like 'train_name', 'departure', 'arrival'
+            cursor.execute(
+                "INSERT INTO yourapp_trainschedule (train_name, departure, arrival) VALUES (%s, %s, %s)",
+                [
+                    request.data.get("train_name"),
+                    request.data.get("departure"),
+                    request.data.get("arrival"),
+                ],
+            )
+        return HttpResponse(status=status.HTTP_200_OK)
 
 
 @api_view(["GET", "PUT", "DELETE"])
 def train_schedule_detail(request, id):
-    train_schedule = get_object_or_404(TrainSchedule, pk=id)
-    if request.method == "GET":
-        serializer = TrainScheduleSerializer(train_schedule)
-        return Response(serializer.data)
-    elif request.method == "PUT":
-        serializer = TrainScheduleSerializer(train_schedule, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-    elif request.method == "DELETE":
-        train_schedule.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    with connection.cursor() as cursor:
+        if request.method == "GET":
+            cursor.execute("SELECT * FROM yourapp_trainschedule WHERE id = %s", [id])
+            schedule = cursor.fetchone()
+            if schedule:
+                return JsonResponse(
+                    dict(zip([col[0] for col in cursor.description], schedule))
+                )
+            else:
+                return HttpResponse(status=404)
+        elif request.method == "PUT":
+            cursor.execute(
+                "UPDATE yourapp_trainschedule SET train_name = %s, departure = %s, arrival = %s WHERE id = %s",
+                [
+                    request.data.get("train_name"),
+                    request.data.get("departure"),
+                    request.data.get("arrival"),
+                    id,
+                ],
+            )
+            return HttpResponse(status=status.HTTP_200_OK)
+        elif request.method == "DELETE":
+            cursor.execute("DELETE FROM yourapp_trainschedule WHERE id = %s", [id])
+            return HttpResponse(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view(["GET", "POST"])
 def order_list(request):
     if request.method == "GET":
-        queryset = Order.objects.all()
-        serializer = OrderSerializer(queryset, many=True)
-        return Response(serializer.data)
-    if request.method == "POST":
-        serializer = OrderSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(request.data, status=status.HTTP_200_OK)
-        else:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM yourapp_order")
+            orders = dictfetchall(cursor)
+        return JsonResponse(orders, safe=False)
+    elif request.method == "POST":
+        with connection.cursor() as cursor:
+            # Assuming Order has fields like 'product_id', 'quantity'
+            cursor.execute(
+                "INSERT INTO yourapp_order (product_id, quantity) VALUES (%s, %s)",
+                [request.data.get("product_id"), request.data.get("quantity")],
+            )
+        return HttpResponse(status=status.HTTP_200_OK)
 
 
 @api_view(["GET", "PUT", "DELETE"])
 def order_detail(request, id):
-    order = get_object_or_404(Order, pk=id)
-    if request.method == "GET":
-        serializer = OrderSerializer(order)
-        return Response(serializer.data)
-    elif request.method == "PUT":
-        serializer = OrderSerializer(order, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-    elif request.method == "DELETE":
-        order.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    with connection.cursor() as cursor:
+        if request.method == "GET":
+            cursor.execute("SELECT * FROM yourapp_order WHERE id = %s", [id])
+            order = cursor.fetchone()
+            if order:
+                return JsonResponse(
+                    dict(zip([col[0] for col in cursor.description], order))
+                )
+            else:
+                return HttpResponse(status=404)
+        elif request.method == "PUT":
+            cursor.execute(
+                "UPDATE yourapp_order SET product_id = %s, quantity = %s WHERE id = %s",
+                [request.data.get("product_id"), request.data.get("quantity"), id],
+            )
+            return HttpResponse(status=status.HTTP_200_OK)
+        elif request.method == "DELETE":
+            cursor.execute("DELETE FROM yourapp_order WHERE id = %s", [id])
+            return HttpResponse(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view(["GET", "POST"])
 def bill_list(request):
     if request.method == "GET":
-        queryset = Bill.objects.all()
-        serializer = BillSerializer(queryset, many=True)
-        return Response(serializer.data)
-    if request.method == "POST":
-        serializer = BillSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(request.data, status=status.HTTP_200_OK)
-        else:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM yourapp_bill")
+            bills = dictfetchall(cursor)
+        return JsonResponse(bills, safe=False)
+    elif request.method == "POST":
+        with connection.cursor() as cursor:
+            # Assuming Bill has fields like 'order_id', 'amount'
+            cursor.execute(
+                "INSERT INTO yourapp_bill (order_id, amount) VALUES (%s, %s)",
+                [request.data.get("order_id"), request.data.get("amount")],
+            )
+        return HttpResponse(status=status.HTTP_200_OK)
 
 
 @api_view(["GET", "PUT", "DELETE"])
 def bill_detail(request, id):
-    bill = get_object_or_404(Bill, pk=id)
-    if request.method == "GET":
-        serializer = BillSerializer(bill)
-        return Response(serializer.data)
-    elif request.method == "PUT":
-        serializer = BillSerializer(bill, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-    elif request.method == "DELETE":
-        bill.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    with connection.cursor() as cursor:
+        if request.method == "GET":
+            cursor.execute("SELECT * FROM yourapp_bill WHERE id = %s", [id])
+            bill = cursor.fetchone()
+            if bill:
+                return JsonResponse(
+                    dict(zip([col[0] for col in cursor.description], bill))
+                )
+            else:
+                return HttpResponse(status=404)
+        elif request.method == "PUT":
+            cursor.execute(
+                "UPDATE yourapp_bill SET order_id = %s, amount = %s WHERE id = %s",
+                [request.data.get("order_id"), request.data.get("amount"), id],
+            )
+            return HttpResponse(status=status.HTTP_200_OK)
+        elif request.method == "DELETE":
+            cursor.execute("DELETE FROM yourapp_bill WHERE id = %s", [id])
+            return HttpResponse(status=status.HTTP_204_NO_CONTENT)
